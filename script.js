99c3c2da010cc716a7e3b721e9116ab3b039bf4f
SmitHumber <yellowpieorangesword14@gmail.com>
2026-07-19 20:27:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Understanding and Creating Objects
 
 let student = {
     name: "Smit",
@@ -12,6 +13,7 @@ let student = {
 console.log(student.name);
 console.log(student.age);
 
+// Using Destructuring Assignment
 
 student.studentInfo();
 
@@ -25,6 +27,7 @@ let {num1, num2} = number;
 console.log(num1);
 console.log(num2);
 
+// The Spread Operator
 
 let studentCourse1 = {a: "CPAN 133"};
 let studentCourse2 = {b: "CPAN 113"};
@@ -32,3 +35,21 @@ let studentCourse2 = {b: "CPAN 113"};
 let graduationYear = { ...studentCourse1, ...studentCourse2};
 
 console.log(graduationYear);
+
+// Object Methods
+
+let student2 = {
+    name: "Smit",
+    age: 22,
+    enrolled: true,
+    course: ["CPAN 113", "CPAN 113", "CPAN 111"],
+    studentInfo: function() {
+        console.log("My name is " + this.name, "I am " + this.age, "This is my classes ", this.course);
+    },
+    studnetGrad: function() {
+        this.age += 5;
+        console.log(`2030 when I gradurated I will be ${this.age}`);
+    }
+};
+
+student2.studnetGrad();
